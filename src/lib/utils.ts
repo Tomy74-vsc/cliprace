e2656ac3264b01e13205b5ca1e881e4799367fb3
Tom <tomrevret@gmail.com>
2025-09-20 16:45:38 +0200

    home page

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export const currency = (cents: number, currency = "EUR") =>
-  new Intl.NumberFormat("fr-FR", { style: "currency", currency }).format(cents / 100);
